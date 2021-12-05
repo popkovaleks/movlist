81704af230ce54796fdd3226c9af948541a8d4be
Aleksandr <popkov.aleks.v@yandex.ru>
2021-12-05 13:03:53 +0500

    migrations and api folder created

---
+++ movlist/movlist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Movie(models.Model):
+
+    name = models.CharField(
+        max_length=500,
+    )
+
+    director = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True
+    )
+
+    year = models.IntegerField(
+        null=True
+    )
